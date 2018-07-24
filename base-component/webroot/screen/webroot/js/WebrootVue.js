963d116659636884baf1e79ee90f97ccd5ce122f
mrovnanik <rovnanik.michal@gmail.com>
2018-07-24 14:35:05 +0200

    Disabled extra logging.

--- base-component/webroot/screen/webroot/js/WebrootVue.js
+++ base-component/webroot/screen/webroot/js/WebrootVue.js
@@ -1472,7 +1472,7 @@ moqui.webrootVue = new Vue({
                 return
             }
 
-            console.log(`Setting locale to: ${JSON.stringify(message)}`);
+            /*console.log(`Setting locale to: ${JSON.stringify(message)}`);*/
             moqui.i18localization.setLocaleMessage(localeToUse, message);
         });
     },
